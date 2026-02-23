dbc723445409894013f3f608d14c5da5f5d3b102
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-02-23 09:11:44 +0800

    try fix: Video jumps back several seconds when switching to popup player

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -858,9 +858,6 @@ public final class Player implements
         }
 
         if (oldPlayerType != playerType && playQueue != null) {
-            // If playerType changes from one to another we should reload the player
-            // (to disable/enable video stream or to set quality)
-            setRecovery();
             reloadPlayQueueManager();
         }
 
