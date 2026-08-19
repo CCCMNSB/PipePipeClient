69730f36a47abcd4e362eb7309ba0444a459ea20
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-08-20 05:43:34 +0800

    fix: imported subscriptions are hidden when channel details are skipped

--- app/src/main/java/org/schabi/newpipe/database/subscription/SubscriptionEntity.java
+++ app/src/main/java/org/schabi/newpipe/database/subscription/SubscriptionEntity.java
@@ -15,6 +15,7 @@ import org.schabi.newpipe.util.Constants;
 import static org.schabi.newpipe.database.subscription.SubscriptionEntity.SUBSCRIPTION_SERVICE_ID;
 import static org.schabi.newpipe.database.subscription.SubscriptionEntity.SUBSCRIPTION_TABLE;
 import static org.schabi.newpipe.database.subscription.SubscriptionEntity.SUBSCRIPTION_URL;
+import static org.schabi.newpipe.extractor.channel.ChannelExtractor.UNKNOWN_SUBSCRIBER_COUNT;
 
 @Entity(tableName = SUBSCRIPTION_TABLE,
         indices = {@Index(value = {SUBSCRIPTION_SERVICE_ID, SUBSCRIPTION_URL}, unique = true)})
@@ -69,6 +70,7 @@ public class SubscriptionEntity {
         result.setServiceId(item.getServiceId());
         result.setUrl(item.getUrl());
         result.setName(item.getName());
+        result.setSubscriberCount(UNKNOWN_SUBSCRIBER_COUNT);
         return result;
     }
 
@@ -149,7 +151,8 @@ public class SubscriptionEntity {
     public ChannelInfoItem toChannelInfoItem() {
         final ChannelInfoItem item = new ChannelInfoItem(getServiceId(), getUrl(), getName());
         item.setThumbnailUrl(getAvatarUrl());
-        item.setSubscriberCount(getSubscriberCount());
+        item.setSubscriberCount(getSubscriberCount() == null
+                ? UNKNOWN_SUBSCRIBER_COUNT : getSubscriberCount());
         item.setDescription(getDescription());
         return item;
     }
